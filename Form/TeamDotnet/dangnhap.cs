692ff04b8dde291f48fd85000016a139c1c8824f
chanhtam19 <74192319+chanhtam19@users.noreply.github.com>
2020-11-12 11:26:35 +0700

    form_dangnhap
    tui moi tao form đăng nhap6

--- Form/TeamDotnet/dangnhap.cs
+++ Form/TeamDotnet/dangnhap.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TeamDotnet
 {
-    public partial class Form1 : Form
+    public partial class dangnhap : Form
     {
-        public Form1()
+        public dangnhap()
         {
             InitializeComponent();
         }
