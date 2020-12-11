777ba7a512bb1fad86a6997b734478109e8f330c
Thanh <ltt.1112000@gmail.com>
2020-12-11 16:37:39 +0700

    .

--- Main/SQLQuery1.sql
+++ Main/SQLQuery1.sql
@@ -1,27 +1,26 @@
-﻿create database QuanLyQuanMyCay
+create database QuanLyQuanMyCay
 create table NhanVien(
-	Id int identity (1,1) ,
-	MaNV  varchar(5) primary key,
+	MaNV char(10) primary key,
 	TenNV nvarchar(30) not null,
 	NgaySinh smalldatetime not null,
 	SDT int not null,
 	DiaChi nvarchar(30),
 	ChucVu nvarchar(20) not null,
 	GioiTinh nvarchar(3),
-	NgayVaoLam smalldatetime not null,
-	CaLam varchar(5) 
+	NgayVaoLam smalldatetime not null
 )
-
 create table TaiKhoan(
-	MaNV varchar(5) not null,
+	MaNV char(10) not null,
 	TaiKhoan varchar(20) unique,
 	MatKhau varchar(30) not null,
 	PhanQuyen int not null,
-	constraint fk_taikhoan_manv foreign key (MaNV) references NhanVien(MaNV)
+	constraint fk_taikhoan_manv foreign key (MaNV) references NhanVien(MaNV),
+	constraint pk_taikhoan primary key(MaNV,taikhoan)
 )
+
 create table PhieuLuong(
-	MaPL varchar(5) primary key,
-	MaNV varchar(5) not null,
+	MaPL char(10) primary key,
+	MaNV char(10) not null,
 	SoNgayLam int default 0,
 	TamUng money default 0,
 	TienPhat money default 0,
@@ -31,69 +30,70 @@ create table PhieuLuong(
 	ThangLuong varchar(20) not null,
 	constraint fk_phieuluong_manv foreign key (MaNV) references NhanVien(MaNV) 
 )
-select MAX(CAST(RIGHT(MaNV,3) as int)) from NhanVien
 create table NguyenLieu(
-	MaNL varchar(5) primary key,
+	MaNL char(10) primary key,
 	TenNL varchar(30) not null,
 	DVT varchar(10) not null,
-	MaNCC varchar(5) not null,
+	MaNCC char(10) not null,
 	constraint fk_nguyenlieu_mancc foreign key (MaNCC) references NhaCungCap(MaNCC)
 )
 create table NhaCungCap(
-	Id int identity(1,1),
-	MaNCC varchar(5) primary key,
+	MaNCC char(10) primary key,
 	TenNCC varchar(30) not null,
 	DiaChi varchar(30) not null,
 	SDT int not null
 )
 create table PhieuNhap(
-	Id int identity(1,1),
-	MaPN varchar(5) primary key,
+	MaPN char(10) primary key,
 	NgayNhap smalldatetime not null,
-	MaNV varchar(5) not null,
+	MaNV char(10) not null,
 	constraint fk_phieunhap_manv foreign key (MaNV) references NhanVien(MaNV)
 )
 create table ChiTietPhieuNhap(
-	MaPN varchar(5) not null,	
-	MaNL varchar(5) not null,
-	DonGia money not null,
+	MaPN char(10) not null,	
+	MaNL char(10) not null,
 	SoLuong int default 0,
 	ThanhTien money default 0,
 	constraint fk_chitietphieunhap_mapn foreign key (MaPN) references PhieuNhap(MaPN),
 	constraint fk_chitietphieunhap_manl foreign key (MaNL) references NguyenLieu(MaNL)
 )
 create table PhieuXuat(
-	Id int identity(1,1),
-	MaPX varchar(5) primary key,
-	MaNV varchar(5) not null,
+	MaPX char(10) primary key,
+	MaNV char(10) not null,
 	NgayXuat smalldatetime not null,
 	constraint fk_phieuxuat_manv foreign key (MaNV) references NhanVien(MaNV)
 )
 create table ChiTietPhieuXuat(
-	MaPX varchar(5) not null,
-	MaNL varchar(5) not null,
+	MaPX char(10) not null,
+	MaNL char(10) not null,
 	SoLuong int,
 	constraint fk_chitietphieuxuat_mapx foreign key (MaPX) references PhieuXuat(MaPX),
 	constraint fk_chitietphieuxuat_manl foreign key (MaNL) references NguyenLieu(MaNL)
 )
 create table Mon(
-	Id int identity(1,1),
-	MaMon varchar(5) primary key,
+	MaMon char(10) primary key,
 	TenMon nvarchar(20) not null unique,
 	DonGia smallmoney default 0
 )
 create table HoaDon(
-    Id int identity(1,1),
-	MaHD varchar(5) primary key,
+	MaHD char(10) primary key,
 	NgayLapHD smalldatetime not null,
-	MaNV varchar(5) not null,
+	MaNV char(10) not null,
 	constraint fk_hoadon_manv foreign key (MaNV) references NhanVien(MaNV)
 )
 create table ChiTietHoaDon(
-	MaMon varchar(5) not null,
+	MaMon char(10) not null,
 	SoLuong int not null,
 	ThanhTien money not null,
-	MaHD varchar(5) not null,
+	MaHD char(10) not null,
 	constraint fk_chitiethoadon_mamon foreign key (MaMon) references Mon(MaMon),
 	constraint fk_chitiethoadon_mahd foreign key (MaHD) references HoaDon(MaHD)
 )
+
+
+insert into NhanVien values ('NV01',N'Nhan Vien',11/26/2000,190000,N'Vinh Long',N'Bep',N'Nam',11/26/2000)
+insert into NhanVien values ('NV02',N'Nhan Vien',11/26/2000,190000,N'Vinh Long',N'Bep',N'Nam',11/26/2000)
+
+update NhanVien set SDT = 160000 where MaNV = 'NV01'
+
+select * from NhanVien
---
+++ Main/formhethong/hethong.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formhethong
+{
+    public partial class hethong : Form
+    {
+        public hethong()
+        {
+            InitializeComponent();
+        }
+
+        private void dsnhanvienToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dsnhanvien ds = new dsnhanvien();
+            ds.MdiParent = this;
+            ds.Show();
+        }
+    }
+}
